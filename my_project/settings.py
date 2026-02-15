1efeb2a507039b5ab8e260696fafb95a693893d1
clarkey164 <s.clarke0896@gmail.com>
2026-02-15 19:16:08 +0000

    static files added

--- my_project/settings.py
+++ my_project/settings.py
@@ -47,6 +47,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -140,3 +141,9 @@ STATIC_URL = 'static/'
 
 LOGIN_REDIRECT_URL = '/reservations/'
 LOGOUT_REDIRECT_URL = 'home/'
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static'),
+]
